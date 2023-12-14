33b25cfbd864568605d8b4bcf417921e58d28bbf
miguel cosano <miguel.cosano@babelgroup.com>
2023-12-15 00:21:35 +0100

    Some changes

--- local_cache.js
+++ local_cache.js
@@ -2,21 +2,26 @@ const NodeCache = require('node-cache');
 
 class CacheLocal {
     static _instance;
+    cache;
 
     constructor(ttlSeconds) {
-        console.log("CacheLocal created")
-        this.cache = new NodeCache({
-            stdTTL: ttlSeconds,
-            checkperiod: ttlSeconds * 0.2,
-            useClones: false
-        });
+        console.log("CacheLocal created");
+        // Initialize cache only if it's not already initialized
+        if (!this.cache) {
+            this.cache = new NodeCache({
+                stdTTL: ttlSeconds,
+                checkperiod: ttlSeconds * 0.2,
+                useClones: false
+            });
+        }
     }
 
-    static getInstance() {
+    static getInstance(ttlSeconds) {
         if (!CacheLocal._instance) {
-            CacheLocal._instance = new CacheLocal(0);
+            CacheLocal._instance = new CacheLocal(ttlSeconds);
+            console.log("CacheLocal instance created");
         }
-        console.log("CacheLocal instance returned")
+        console.log("CacheLocal instance returned");
         return CacheLocal._instance;
     }
 
@@ -29,4 +34,4 @@ class CacheLocal {
     }
 }
 
-module.exports = CacheLocal.getInstance();
+module.exports = CacheLocal.getInstance(0);
