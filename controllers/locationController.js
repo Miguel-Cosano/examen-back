5155bebf7ce40cbdb647c82b5ff8f3cf62fc6b7e
miguel cosano <miguel.cosano@babelgroup.com>
2023-12-14 13:45:41 +0100

    Some changes

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,9 +1,9 @@
-const ServiceCarbono = require('../services/locationService');
-const serviceCarbono = new ServiceCarbono();
+const ServiceLocation = require('../services/locationService');
+const serviceLocation = new ServiceLocation();
 
 const getCoordinatesFromPostalCode = async (req, res, next) => {
     try {
-        const coordenadas = await serviceCarbono.getCoordenadasByCodPostal(req.query.codPostal)
+        const coordenadas = await serviceLocation.getCoordenadasByCodPostal(req.query.codPostal)
         res.status(200).json(coordenadas)
     } catch (error) {
         res.status(401).send({success: false, message: 'No se ha podido obtener las coordenadas para el código postal ' + req.query.codPostal});
@@ -12,7 +12,7 @@ const getCoordinatesFromPostalCode = async (req, res, next) => {
 
 const getHuellaCarbono = async (req, res) => {
     try {
-        const huella = await serviceCarbono.getHuellaCarbono(req.query.userLat,req.query.userLong, req.query.codPostalProducto);
+        const huella = await serviceLocation.getHuellaCarbono(req.query.userLat,req.query.userLong, req.query.codPostalProducto);
         res.status(200).send(huella);
     } catch (error) {
         res.status(500).send({success: false, message: error.message});
