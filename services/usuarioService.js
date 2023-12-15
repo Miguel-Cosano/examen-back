308c1333a6591540083bfcf571f91043f27ab950
miguel cosano <miguel.cosano@babelgroup.com>
2023-12-15 01:13:31 +0100

    Some changes

--- services/usuarioService.js
+++ services/usuarioService.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const ServiceProducto = require('../services/productoService');
 const serviceProducto = new ServiceProducto();
 
-var cache = require('local_cache.js');
+var cache = require('../local_cache');
 function formatarFecha(fecha) {
     const dia = fecha.getDate().toString().padStart(2, '0');
     const mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // Se suma 1 porque los meses comienzan desde 0
